e3ea415748ba7607e9aa43bf37c04ac8eb6d878b
ApaekshitRawat <Apekshitrawat@gmail.com>
2023-03-31 15:02:41 +0530

    added partials

--- app.js
+++ app.js
@@ -6,14 +6,50 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware & static files
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  console.log("Logger Middleware");
+  console.log("host", req.hostname);
+  console.log("path", req.path);
+  console.log("method", req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in the next middleware");
+  next();
+});
+
 app.get("/", (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to get a job in 2023",
+      snippet: "I dont know :)",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   // res.send('<p>about page</p>');
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "about" });
 });
 
 // redirects
